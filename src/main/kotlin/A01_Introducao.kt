4dae64d9cf5ba54c775bc2f1f9afb02509286b4d
Parisotto <parisotto@gmail.com>
2024-08-30 14:46:36 -0300

    first commit

---
+++ src/main/kotlin/A01_Introducao.kt
@@ -0,0 +1,28 @@
+fun main() {
+    // Aula 1: Primeiro contato
+
+    println("Olá, mundo!")
+
+    print("Edson")
+    print(" ")
+    print("Parisotto")
+
+    println()
+
+    var nome = "Edson\nLuiz\nParisotto"
+    println(nome)
+
+    // var nome = "Não posso redeclarar uma variável"
+    nome = "Edson"
+    print("Olá, " + nome + "\n")
+
+    var sobrenome = "Parisotto"
+    println("Como vai, $nome $sobrenome?")
+
+    val variavelEstatica = "Bom dia"
+    // variavelEstatica = "Não posso reatribuir!"
+
+    val nomeCompleto = nome + " Luiz " + sobrenome
+    println(nomeCompleto)
+    println("$variavelEstatica, ${nomeCompleto}!")
+}
---
+++ src/main/kotlin/A04_Exercicio.kt
@@ -0,0 +1,12 @@
+/*
+    Exercício de reforço: Variáveis, tipo de dados e operadores aritméticos
+
+    a. Crie um novo arquivo chamado A04_Exercicio.kt com uma função main.
+    b. Declare uma variãvel mutável capaz de armazenar seu nome completo.
+    c. Declare uma variável de texto sem valor algum.
+    d. Declare uma variável imutável com o menor tipo de dado possível capaz de armazenar o número que você calça
+    e. Declare uma variável capaz de armazenar o PIB do Brazil este ano de 2024 que é cerca de 10.900.000.000.000 (10,9 trilhões)
+    f. Declare uma variável capaz de armazenar a população do Brasil em 2024 que é cerca de 212.600.000 (212,6 milhões)
+    g. Imprima no terminal o valor do PIB per capita.
+    h. Rode seu prograa de maneira que não possua erros de compilação ou execução.
+*/
