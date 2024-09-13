7a2cb77372fb2c3107fc22ef01fe8bc134608b56
Parisotto <parisotto@gmail.com>
2024-09-13 12:19:26 -0300

    Atualização

--- src/main/kotlin/A08_Exercicio.kt
+++ src/main/kotlin/A08_Exercicio.kt
@@ -1,8 +1,10 @@
 /*
-Aula 8: Exercício de reforço
-Métodos e Funções
+    Aula 8: Exercício de reforço
+    Métodos e Funções
+*/
 
-a. Escreva uma função que seja capaz de receber a quantidade de anos e
+/*
+// a. Escreva uma função que seja capaz de receber a quantidade de anos e
     transformar em meses, dias, horas, minutos e segundos. Saída desejada:
                 2 anos equivalem a:
                     24 meses
@@ -10,15 +12,21 @@ a. Escreva uma função que seja capaz de receber a quantidade de anos e
                     17520 horas
                     1051200 minutos
                     63072000 segundos
-b. Escreva uma função capaz de receber uma sring e retornar a quantidade de caracteres.
-c. Escreva uma função capaz de calcular o cubo de um número inteiro (cubo = n*n*n).
-d. Escreva uma função capaz de receber milhas e converter em km (1 milha = 1.6 km).
-e. Escreva um programa que seja capaz de receber uma string e fazer a troca de todas
+*/
+
+// b. Escreva uma função capaz de receber uma sring e retornar a quantidade de caracteres.
+
+// d. Escreva uma função capaz de receber milhas e converter em km (1 milha = 1.6 km).
+
+// c. Escreva uma função capaz de calcular o cubo de um número inteiro (cubo = n*n*n).
+
+/*
+// e. Escreva um programa que seja capaz de receber uma string e fazer a troca de todas
     as letras "a" ou "A" por "x". Observando que:
         1. Nâo deve existir lógica dentro da função main.
             Main só deve ser usada como ponto de entrada.
         2. Escrever uma função para a troca de letras e impressão do valor final.
         3. String final deve estar com todas as letras minúsculas.
-f. Sobre as funções criadas nos exercícios b, c e d: é possível transformá-las
-    em funões de uma única linha? Se sim, transforme-as.
 */
+
+// f. Sobre as funções criadas nos exercícios b, c e d: é possível transformá-las em funões de uma única linha? Se sim, transforme-as.
