6cfde367a1ceb396f9596db12a4963f352da34d5
paulo.antunes <phantunes@gmail.com>
2024-09-27 17:46:37 -0300

    .

---
+++ src/main/kotlin/A08_Exercicios.kt
@@ -0,0 +1,28 @@
+/*
+    Aula 8: Exercício de reforço
+    Métodos e funções
+ */
+
+/* 1. Escreva uma função que seja capaz de receber a quantidade de anos e tranformar em meses, dias, hras, minutos e segundos. Sída desejada:
+    2 anos equivalem a:
+        24 meses
+        730 dias
+        17520 horas
+        1051200 minutos
+        63072000 segundos
+ */
+
+// 2. Escreva uma função capaz de receber um string e retornar a quantidade de carcteres
+
+// 3. Escreva uma função capaz de calcular o cubo de um número inteiro (cubo = n*n*n)
+
+// 4. Escreva uma função capaz de receber uma medida em milhas e converter em km (1 milha = 1.6 km)
+
+/* 5. Escreva um programa que seja capaz de receber uma string e fazer a troca todas as letras "a" ou "A" por "x". Observando que:
+    a. Não deve existir lógica dentro da função main. Main só deve ser usada como ponto de entrada.
+    b. Escrever uma função para a troca de letras e impressão do valor final.
+    c. String final deve estar com todas as letras maiúsculas
+    Exemplo: Entrada = "Programação em Kotlin" - Saída = "PROGRxMxÇxO EM KOTLIN"
+ */
+
+// 6. Sobre as funções criadas nos exercícios 2, 3 e 4, é possível transfomá-las em fuções de uma única linha? Se sim, trasforme-as.
