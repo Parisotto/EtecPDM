56a43b5e6d259737e66948c3559de397c11aa911
paulo.antunes <phantunes@gmail.com>
2024-09-27 17:06:14 -0300

    .

--- src/main/kotlin/A07_FuncoesMatematicas.kt
+++ src/main/kotlin/A07_FuncoesMatematicas.kt
@@ -1,28 +1,8 @@
-<<<<<<< HEAD
 import kotlin.math.*
 
 // Aula 7: Funções Matem-áticas
 
-fun main(){
-    println(max(5, 10))
-    println(min(5, 10))
-    println(sqrt(49f))
-
-    println(PI)
-    println(E)
-
-    println(round(35465.4000009987898789766))
-    println(round(PI))
-    println(round(E))
-
-    val PI = 3.14159
-    print(PI)
-=======
-import kotlin.math.*
-
-// Aula 7: Funções Matem-áticas
-
-fun main(){
+fun main() {
     println(max(5, 10))
     println(min(5, 10))
     println(sqrt(49f))
