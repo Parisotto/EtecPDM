a628d6a034e9fba860abc16552dfc826ffc4e601
Parisotto <parisotto@gmail.com>
2024-09-29 13:14:31 -0300

    .

---
+++ src/main/kotlin/A18_Exercicios.kt
@@ -0,0 +1,44 @@
+/*
+    Aula 18: Exercícios de reforço
+    Controle de Fluxo: for(), while(), do while(), continue, break
+ */
+
+// CONTROLE DE FLUXO - for()
+
+// 1. Imprima os núeros de 1 a 50 na mesma linha em ordem crescente. Saída esperada:
+//      1 2 3 4 5 6 7 8 9 10 ....... 49 50
+
+// 2. Iprima os números de 1 a 50 na mesma linha em ordem decrescente. Saída esperada:
+//      50 49 48 47 46 45 ......... 3 2 1
+
+// 3. Usando a resolução do exercício 1, não imprima os números múltiplos de 5.
+
+// 4. Faça a soma de todos os números no intervalo de 1 a 500.
+
+/* 5. Escreva um programa capaz de receber um número inteiro N e imprimir uma escada de tamanho N usando o caracter cerquilha (#).
+      Exemplo: se a entrada for 5, teremos:
+            #
+            ##
+            ###
+            ####
+            #####
+ */
+
+// CONTROLE DE FLUXO while() e do while()
+
+// 1. Considere uma caixa d'água de 2 mil litros. Rômulo gostaria de encher a caixa d1água de 7 litros. Quantos balões cabem na caixa d'água sem que o volume seja excedido? Crie uma rotina usando while() ou do while() que retorne a resposta.
+
+/* 2. FizzBuzz. Imprima os números de 1 a 50 em ordem crescente de acordo com a regra abaixo. Use while() ou do while()
+      a. Quando um número for divisível por 3, imprimir Buzz.
+      b. Quando um número for divisível por 5, imprimir Fizz.
+      c. Quando um número for divisível por 3 e 5, imprimir FizzBuzz
+ */
+
+/* 3. Escreva um código, usando while() ou do while(), capaz de receber um texto e imprimí-lo invertido
+      Exemplo de entrada: Meu nome é Asdrúbal
+      Exemplo de saída esperada: labúrdsA é emon ueM
+*/
+
+/* 4. Escreva uma função que recebe uma string, verifica se existe a mesma quantidade de caracteres "x" e "o" e retorna true ou false. Caso não exista nem "x" nem "o", retornar false. Use while() ou do while()
+ */
+
