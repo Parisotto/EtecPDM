cec863afbf32c4eb15fefa0aeabeb6416e26c7e8
parisotto <parisotto@gmail>
2024-10-05 08:48:19 -0300

    Exercícios 05/10/2024

---
+++ src/main/kotlin/A13_Exercicios.kt
@@ -0,0 +1,10 @@
+/*
+    Aula 13: Exercícios de reforço
+    - Controle de fluxo: "if()"
+    - Leitura de linha: "readln()
+    - Conversão de inteiro: "toInt()"
+ */
+
+// 1. Escreva uma rotina capaz de ler dois números inteiros maiores que zero que representam os lados de uma girua geométrica quadrada ou retangular. Informar se é um quadrado ou retangulo.
+
+// 2. Escreva uma função capaz de ler três números inteiros maiores que zero que representam os lados de um triângulo. Informar se é um triângulo equilátero (todos os lados iguais), isósceles (apenas dois lados são iguais) ou escaleno (os trÊs lados são diferentes.
