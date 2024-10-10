099131c123270a5b79f4f809bc074d8832be1b98
Parisotto <parisotto@gmail.com>
2024-10-09 22:21:49 -0300

    .

--- src/main/kotlin/A18_Exercicios_Resolvidos.kt
+++ src/main/kotlin/A18_Exercicios_Resolvidos.kt
@@ -14,7 +14,12 @@ fun main() {
     //litros2()
     //fizzBuzz()
     //inverter("Meu nome é Asdrúbal")
-    println(contaXeO("xxXxoooO"))
+    println(contaXeO("xxxooo")) // true
+    println(contaXeO("xxXxoooO"))  // true
+    println(contaXeO("xaobocx")) // true
+    println(contaXeO("xxxo")) // false
+    println(contaXeO("abcdef")) // false
+    println(contaXeO(" ")) // false
 }
 
 // CONTROLE DE FLUXO - for()
@@ -84,12 +89,12 @@ fun escada(n: Int){
 // 1. Considere uma caixa d'água de 2 mil litros. Rômulo gostaria de encher a caixa d'água com balões de água de 7 litros. Quantos balões cabem na caixa d'água sem que o volume seja excedido? Crie uma rotina usando while() ou do while() que retorne a resposta.
 fun litros(){
     var caixa = 2000
-    var baloes = 0
+    var numBaloes = 0
     do {
         caixa -= 7
-        baloes++
+        numBaloes++
     } while (caixa >= 7)
-    println("Cabem $baloes balôes.")
+    println("Cabem $numBaloes balôes.")
 }
 
 fun litros2(){
@@ -133,22 +138,25 @@ fun inverter(texto:String){
 /* 4. Escreva uma função que recebe uma string, verifica se existe a mesma quantidade de caracteres "x" e "o" e retorna true ou false. Caso não exista nem "x" nem "o", retornar false. Use while() ou do while()
  */
 fun contaXeO(texto: String): Boolean {
-    var i = 0
-    var x = 0
-    var o = 0
     var txt = texto.lowercase()
+    var numX = 0
+    var numO = 0
+
+    var i = 0
     do {
         if (txt[i] == 'x'){
-            x++
+            numX++
         } else if (txt[i] == 'o'){
-            o++
+            numO++
         }
         i++
     } while (i < txt.length)
-
-    if (x == o && x != 0) {
+/*
+    if (numX == numO && numX != 0) {
         return true
     } else {
         return false
     }
+ */
+    return numX == numO && numX != 0
 }
---
+++ src/main/kotlin/A21_Metodo_let.kt
@@ -0,0 +1,18 @@
+// Aula 21: Método ".let"
+
+fun main() {
+    val str: String? = "hello"
+
+    if (str != null){
+        str.uppercase()
+        str.length
+        println("\\\\ Á variável 'str' contém \"$str\" \\")
+    }
+
+    str?.let {
+        // Aqui dentro, a variável 'str' passa a ser 'it'
+        println("A variável $it NÃO é nula")
+        println(it.uppercase())
+        println(it.length)
+    }
+}
