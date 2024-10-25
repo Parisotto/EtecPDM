e00a6fbe08e778e8787ff9be06a7b24d0c379f06
parisotto <parisotto@gmail>
2024-10-25 18:10:49 -0300

    Aula 25/10/2024

---
+++ src/main/kotlin/A19_Excecoes.kt
@@ -0,0 +1,29 @@
+// Aula 19: Exceções: NullPointer e Arithmetic
+
+import java.lang.Exception
+
+fun main() {
+    var str: String? = null
+    // print(str!!.length)
+    str = "Texto"
+    try {
+        println(str[str.length-1])
+        val n = 10 / 0
+        print(str!!.length)
+    } catch (e: NullPointerException){
+        println("A variável é nula!")
+    } catch (e: ArithmeticException) {
+        println("Impossível dividir por zero!")
+    } finally {
+        println("Fechando conexão com BD")
+    }
+    println("fim")
+
+    try {
+        return
+    } catch (e: Exception){
+        println("Exception")
+    } finally {
+        println("Finally")
+    }
+}
---
+++ src/main/kotlin/A20_Elvis.kt
@@ -0,0 +1,21 @@
+// Aula 20: Operador Elvis ?:
+
+fun main() {
+    var str: String? = null
+    if (str == null) {
+        println("A variável é nula")
+    } else {
+        println(str)
+    }
+
+    println(str ?: "Ok. Segundo Elvis, a variável 'str' é nula")
+    println(str ?: 7)
+    var teste = str ?: true
+    println(teste)
+    println(str ?: nulidade())
+    str ?: println(nulidade())
+}
+
+fun nulidade(): String {
+    return "Sim! É nula!"
+}
