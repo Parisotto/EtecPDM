97acd4d3cf8dce5cb7739c18398d71e94e226d54
parisotto <parisotto@gmail>
2024-10-25 17:20:56 -0300

    Aula 25/10/2024

--- src/main/kotlin/A18_Exercicios_while.kt
+++ src/main/kotlin/A18_Exercicios_while.kt
@@ -5,7 +5,10 @@
 
 // CONTROLE DE FLUXO - while()
 fun main(){
-    litros3()
+    // litros3()
+    // fizzBuzz()
+    // inverter("Meu nome é Asdrúbal")
+    println(contaXeO("xxxooo"))
 }
 
 // 1. Considere uma caixa d'água de 2 mil litros. Rômulo gostaria de encher a cahixa d'água com balões de 7 litros. Quantos balões cabem na caixa d'água sem que o volume seja excedido? Crie uma rotina usando while() que retorne a resposta.
@@ -38,11 +41,50 @@ fun litros3(){
         b. Quando um número for divisível por 5, imprimir Fizz.
         c. Quando um número for divisível por 3 e 5 simultaneamente, imprimir FizzBuzz
  */
+fun fizzBuzz(){
+    var i = 1
+    do {
+        if (i % 3 == 0 && i % 5 == 0){
+            print("FizzBuzz ")
+        } else if (i % 3 == 0){
+            print("Buzz ")
+        } else if (i % 5 == 0) {
+            print("Fizz ")
+        } else {
+            print("$i ")
+        }
+        i++
+    } while (i <= 50)
+}
 
 /* 3. Escreva um código, usando while() ou do while(), capaz de receber um texto e imprimí-lo invertido
       Exemplo de entrada: Meu nome é Asdrúbal
       Exemplo de saída esperada: labúrdsA é emon ueM
 */
+fun inverter(texto: String) {
+    // println(texto.reversed())
+    var i = texto.length - 1
+    do {
+        print("${texto[i]}")
+        i--
+    } while (i >= 0)
+}
 
 // 4. Escreva uma função que recebe uma string, verifica se existe a mesma quantidade de caracteres "x" e "o" e retorna true ou false. Caso não exista nem "x" nem "o", retornar false. Use while() ou do while()
+fun contaXeO(texto: String): Boolean {
+    var txt = texto.lowercase()
+    var numX = 0
+    var numO = 0
 
+    var i = 0
+    do {
+        if (txt[i] == 'o') {
+            numO++
+        } else if (txt[i] == 'x') {
+            numX++
+        }
+        i++
+    } while (i < txt.length)
+
+    return numX == numO && numX != 0
+}
